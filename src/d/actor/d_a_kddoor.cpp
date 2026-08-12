a7f0a3926115ea9efd6a306a9f70315aba9eb8cf
bandit31 <bandit31@bandit>
2026-08-12 23:13:30 +0300

    d_a_kddoor: drawSet param_8 1->0 (retail li r0,0x0 at stack 0xc, D10-swept revert was wrong), closeProc-sub roomNo via fopAcM_GetRoomNo (retail lbz-before-lwz order snaps 98.41->100), drop stale Nonmatching markers on 8 now-100% fns

--- src/d/actor/d_a_kddoor.cpp
+++ src/d/actor/d_a_kddoor.cpp
@@ -356,7 +356,6 @@ BOOL dDoor_ssk_c::openProc(dDoor_info_c* i_this) {
 
 /* 00000D84-00000DE8       .text init__15dDoor_ssk_sub_cFv */
 void dDoor_ssk_sub_c::init() {
-    /* Nonmatching */
     static dCcD_SrcCyl body_co_cyl = {
         // dCcD_SrcGObjInf
         {
@@ -462,7 +461,6 @@ void dDoor_ssk_sub_c::closeInit() {
 
 /* 00001024-0000121C       .text closeProc__15dDoor_ssk_sub_cFP12dDoor_info_c */
 BOOL dDoor_ssk_sub_c::closeProc(dDoor_info_c* i_this) {
-    /* Nonmatching */
     if (mTimer > 0) {
         mTimer--;
         return FALSE;
@@ -496,7 +494,7 @@ BOOL dDoor_ssk_sub_c::closeProc(dDoor_info_c* i_this) {
             angle.y += 0x7FFF;
         }
         dComIfGp_particle_setToon(
-            dPa_name::ID_IT_ST_TGSYOKU_SMOKE00, &mPos, &angle, NULL, 0xb9, &mSmokeCb, i_this->current.roomNo
+            dPa_name::ID_IT_ST_TGSYOKU_SMOKE00, &mPos, &angle, NULL, 0xb9, &mSmokeCb, fopAcM_GetRoomNo(i_this)
         );
     }
 
@@ -535,11 +533,10 @@ dDoor_ssk_sub_c::~dDoor_ssk_sub_c() {}
 
 /* 000015A4-00001778       .text drawSet__15dDoor_ssk_sub_cFv */
 BOOL dDoor_ssk_sub_c::drawSet() {
-    /* Nonmatching */
     mpMorf = new mDoExt_McaMorf(
         (J3DModelData*)dComIfG_getObjectRes("Ssk", dRes_INDEX_SSK_BDL_TURU_02_e),
         NULL, NULL, NULL,
-        J3DFrameCtrl::EMode_LOOP, 1.0f, 0, -1, 1,
+        J3DFrameCtrl::EMode_LOOP, 1.0f, 0, -1, 0,
         NULL,
         0x00000000,
         0x11020203
@@ -556,7 +553,7 @@ BOOL dDoor_ssk_sub_c::drawSet() {
     mpMorf2 = new mDoExt_McaMorf(
         (J3DModelData*)dComIfG_getObjectRes("Ssk", dRes_INDEX_SSK_BDL_KTANA_00_e),
         NULL, NULL, NULL,
-        J3DFrameCtrl::EMode_LOOP, 1.0f, 0, -1, 1,
+        J3DFrameCtrl::EMode_LOOP, 1.0f, 0, -1, 0,
         NULL,
         0x00000000,
         0x11020203
@@ -569,7 +566,6 @@ BOOL dDoor_ssk_sub_c::drawSet() {
 
 /* 00001778-00001904       .text calcMtx__15dDoor_ssk_sub_cFP12dDoor_info_cffUc */
 void dDoor_ssk_sub_c::calcMtx(dDoor_info_c* i_this, f32 x, f32 z, u8 arg3) {
-    /* Nonmatching */
     if (mpMorf != NULL) {
         mpMorf->getModel()->setBaseScale(*(Vec*)mScale);
         mpMorf2->getModel()->setBaseScale(*(Vec*)mScale2);
@@ -615,7 +611,6 @@ static BOOL CheckCreateHeap(fopAc_ac_c* i_this) {
 
 /* 00001954-00001B74       .text CreateHeap__10daKddoor_cFv */
 BOOL daKddoor_c::CreateHeap() {
-    /* Nonmatching */
     J3DModelData* modelData = (J3DModelData*)dComIfG_getStageRes("Stage", getBmdName());
     if (modelData == NULL) {
         modelData = (J3DModelData*)dComIfG_getStageRes("Stage", getBmdName2());
@@ -663,7 +658,6 @@ BOOL daKddoor_c::CreateHeap() {
 
 /* 00001B74-00001CD0       .text setEventPrm__10daKddoor_cFv */
 void daKddoor_c::setEventPrm() {
-    /* Nonmatching */
     if (mFrontCheck == 0) {
         m2C6 = 2;
         if (mSsk.m55A == 0xFF) {
@@ -766,7 +760,6 @@ void daKddoor_c::calcMtx() {
 
 /* 00002108-0000220C       .text CreateInit__10daKddoor_cFv */
 BOOL daKddoor_c::CreateInit() {
-    /* Nonmatching */
     if (dComIfG_Bgsp()->Regist(mpBgW, this)) {
         JUT_ASSERT(0x43E, NULL);
     }
@@ -814,7 +807,6 @@ cPhs_State daKddoor_c::create() {
 
 /* 000023CC-00002678       .text demoProc__10daKddoor_cFv */
 void daKddoor_c::demoProc() {
-    /* Nonmatching */
     s32 iVar1 = getDemoAction();
     if (dComIfGp_evmng_getIsAddvance(mStaffId)) {
         switch(iVar1) {
