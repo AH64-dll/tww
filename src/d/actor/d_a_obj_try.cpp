cca4349baac736c9c8034052438f898f224ae072
bandit31 <bandit31@bandit>
2026-08-12 17:10:07 +0300

    d_a_obj_try: restore remaining stub fns (all 49 symbols present, per-TU green)

--- src/d/actor/d_a_obj_try.cpp
+++ src/d/actor/d_a_obj_try.cpp
@@ -346,6 +346,206 @@ s32 Act_c::create_heap() {
     return 0;
 }
 
+/* 0000026C-0000038C       .text init_cc__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::init_cc() {
+    /* Nonmatching */
+}
+
+/* 0000038C-00000428       .text search_sameType__Q28daObjTry5Act_cFPvPv */
+void daObjTry::Act_c::search_sameType(void*, void*) {
+    /* Nonmatching */
+}
+
+/* 00000428-00000528       .text chk_appear__Q28daObjTry5Act_cCFv */
+void daObjTry::Act_c::chk_appear() const {
+    /* Nonmatching */
+}
+
+/* 00000528-000008D8       .text _create__Q28daObjTry5Act_cFv */
+cPhs_State daObjTry::Act_c::_create() {
+    /* Nonmatching */
+}
+
+/* 00000C88-00000D5C       .text _delete__Q28daObjTry5Act_cFv */
+bool daObjTry::Act_c::_delete() {
+    /* Nonmatching */
+}
+
+/* 00000D5C-00000EA8       .text mode_restart_init__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::mode_restart_init() {
+    /* Nonmatching */
+}
+
+/* 00000EA8-00000FFC       .text mode_restart__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::mode_restart() {
+    /* Nonmatching */
+}
+
+/* 00000FFC-00001074       .text mode_wait_init__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::mode_wait_init() {
+    /* Nonmatching */
+}
+
+/* 00001074-000012C4       .text mode_wait__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::mode_wait() {
+    /* Nonmatching */
+}
+
+/* 000012C4-00001344       .text mode_carry_init__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::mode_carry_init() {
+    /* Nonmatching */
+}
+
+/* 00001344-000013D4       .text mode_carry__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::mode_carry() {
+    /* Nonmatching */
+}
+
+/* 000013D4-000014A8       .text mode_drop_init__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::mode_drop_init() {
+    /* Nonmatching */
+}
+
+/* 000014A8-00001518       .text mode_drop__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::mode_drop() {
+    /* Nonmatching */
+}
+
+/* 00001518-000016A0       .text mode_sink_init__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::mode_sink_init() {
+    /* Nonmatching */
+}
+
+/* 000016A0-0000177C       .text mode_sink__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::mode_sink() {
+    /* Nonmatching */
+}
+
+/* 0000177C-00001B58       .text mode_proc_call__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::mode_proc_call() {
+    /* Nonmatching */
+}
+
+/* 00001B58-00001B90       .text cull_set_draw__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::cull_set_draw() {
+    /* Nonmatching */
+}
+
+/* 00001B90-00001BC8       .text cull_set_move__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::cull_set_move() {
+    /* Nonmatching */
+}
+
+/* 00001BC8-00001BE8       .text damaged__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::damaged() {
+    /* Nonmatching */
+}
+
+/* 00001BE8-00001CD8       .text damage_cc_proc__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::damage_cc_proc() {
+    /* Nonmatching */
+}
+
+/* 00001CD8-00001E98       .text damage_bg_proc__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::damage_bg_proc() {
+    /* Nonmatching */
+}
+
+/* 00001E98-00001FEC       .text damage_bg_proc_directly__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::damage_bg_proc_directly() {
+    /* Nonmatching */
+}
+
+/* 00001FEC-00002034       .text chk_sink_water__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::chk_sink_water() {
+    /* Nonmatching */
+}
+
+/* 00002034-000020AC       .text chk_sinkdown_water__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::chk_sinkdown_water() {
+    /* Nonmatching */
+}
+
+/* 000020AC-00002218       .text calc_drop_param__Q28daObjTry5Act_cCFPfPfPf */
+void daObjTry::Act_c::calc_drop_param(float*, float*, float*) const {
+    /* Nonmatching */
+}
+
+/* 00002218-000022F4       .text bound__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::bound() {
+    /* Nonmatching */
+}
+
+/* 000022F4-0000240C       .text se_fall_water__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::se_fall_water() {
+    /* Nonmatching */
+}
+
+/* 0000240C-00002460       .text set_senv__Q28daObjTry5Act_cCFii */
+void daObjTry::Act_c::set_senv(int, int) const {
+    /* Nonmatching */
+}
+
+/* 00002460-00002498       .text cam_lockoff__Q28daObjTry5Act_cCFv */
+void daObjTry::Act_c::cam_lockoff() const {
+    /* Nonmatching */
+}
+
+/* 00002498-00002504       .text set_mtx__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::set_mtx() {
+    /* Nonmatching */
+}
+
+/* 00002504-00002540       .text init_mtx__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::init_mtx() {
+    /* Nonmatching */
+}
+
+/* 00002540-00002718       .text eff_set_bingo__Q28daObjTry5Act_cFbb */
+void daObjTry::Act_c::eff_set_bingo(bool, bool) {
+    /* Nonmatching */
+}
+
+/* 00002718-00002790       .text eff_clr_bingo__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::eff_clr_bingo() {
+    /* Nonmatching */
+}
+
+/* 00002790-000027BC       .text eff_land_smoke__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::eff_land_smoke() {
+    /* Nonmatching */
+}
+
+/* 000027BC-0000280C       .text eff_hit_water_splash__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::eff_hit_water_splash() {
+    /* Nonmatching */
+}
+
+/* 0000280C-00002868       .text make_vib__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::make_vib() {
+    /* Nonmatching */
+}
+
+/* 00002868-00002960       .text check_circle__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::check_circle() {
+    /* Nonmatching */
+}
+
+/* 00002960-00002A90       .text get_water_h__Q28daObjTry5Act_cFv */
+void daObjTry::Act_c::get_water_h() {
+    /* Nonmatching */
+}
+
+/* 00002BB4-00002DA8       .text _execute__Q28daObjTry5Act_cFv */
+bool daObjTry::Act_c::_execute() {
+    /* Nonmatching */
+}
+
+/* 00002DA8-00002EA4       .text _draw__Q28daObjTry5Act_cFv */
+bool daObjTry::Act_c::_draw() {
+    /* Nonmatching */
+}
+
 namespace {
 /* 00002EA4-00002EC4       .text Mthd_Create__Q28daObjTry25@unnamed@d_a_obj_try_cpp@FPv */
 cPhs_State Mthd_Create(void* i_this) {
