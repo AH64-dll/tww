b28b6df1dd00f1bb87b653bdecc6b003cbac5a0d
bandit31 <bandit31@bandit>
2026-08-12 12:57:49 +0300

    gy: park full WIP (header+cpp) to green stub; D4W3-3 reapply in compiling increments (draft in history d245bfd1)

--- include/d/actor/d_a_gy.h
+++ include/d/actor/d_a_gy.h
@@ -7,12 +7,10 @@
 #include "d/d_bg_s_acch.h"
 #include "d/d_particle.h"
 
-class daGy_Ctrl_c;
-
 class daGy_c : public fopEn_enemy_c {
 public:
     void _nodeControl(J3DNode*, J3DModel*);
-    BOOL _createHeap();
+    void _createHeap();
     void setMtx();
     void setAnm();
     void setAtCollision();
@@ -52,44 +50,21 @@ public:
     bool _delete();
 
 public:
-    /* 0x2AC */ s32 m2AC;
+    /* 0x2AC */ u8 m2AC[0x2B0 - 0x2AC];
     /* 0x2B0 */ s32 m2B0;
-    /* 0x2B4 */ f32 m2B4;
-    /* 0x2B8 */ daGy_Ctrl_c* mpCtrl;
-    /* 0x2BC */ cXyz m2BC;
-    /* 0x2C8 */ request_of_phase_process_class mPhs;
-    /* 0x2D0 */ u8 m2D0[0x2D4 - 0x2D0];
+    /* 0x2B4 */ u8 m2B4[0x2D4 - 0x2B4];
     /* 0x2D4 */ mDoExt_McaMorf* mpMorf;
     /* 0x2D8 */ mDoExt_invisibleModel m2D8;
     /* 0x2E0 */ dBgS_Acch mAcch;
     /* 0x4A4 */ dBgS_AcchCir mAcchCir;
-    /* 0x4E4 */ f32 m4E4;
-    /* 0x4E8 */ f32 m4E8;
-    /* 0x4EC */ f32 m4EC;
-    /* 0x4F0 */ f32 m4F0;
-    /* 0x4F4 */ u8 m4F4[0x4F8 - 0x4F4];
-    /* 0x4F8 */ s32 m4F8;
-    /* 0x4FC */ s32 m4FC;
-    /* 0x500 */ s32 m500;
-    /* 0x504 */ f32 m504;
-    /* 0x508 */ f32 m508;
+    /* 0x4E4 */ u8 m4E4[0x50C - 0x4E4];
     /* 0x50C */ dCcD_GObjInf m50C;
     /* 0x604 */ u8 m604[0x624 - 0x604];
     /* 0x624 */ cM3dGSph m624;
     /* 0x638 */ u8 m638[0x8C4 - 0x638];
     /* 0x8C4 */ dCcD_GStts mGStts;
     /* 0x8E4 */ u32 m8E4;
-    /* 0x8E8 */ u8 m8E8[0x8F8 - 0x8E8];
-    /* 0x8F8 */ cXyz m8F8;
-    /* 0x904 */ cXyz m904;
-    /* 0x910 */ u8 m910[0x914 - 0x910];
-    /* 0x914 */ s32 m914;
-    /* 0x918 */ s32 m918;
-    /* 0x91C */ u8 m91C[0x920 - 0x91C];
-    /* 0x920 */ s32 m920;
-    /* 0x924 */ s32 m924;
-    /* 0x928 */ s32 m928;
-    /* 0x92C */ u8 m92C[0x978 - 0x92C];
+    /* 0x8E8 */ u8 m8E8[0x978 - 0x8E8];
     /* 0x978 */ dCcD_GStts m978;
     /* 0x998 */ dCcD_GObjInf m998;
     /* 0xA90 */ u8 mA90[0xAE0 - 0xA90];
