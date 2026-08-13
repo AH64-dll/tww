3db5fdfeac956a1434eb8bdc30c72522789b64e8
bandit31 <bandit31@bandit>
2026-08-13 07:18:16 +0300

    d_a_item: add /* Nonmatching */ to mode_proc_call (99.90 anchor+branch accept-class, R10-flagged marker gap on flip #25); fresh .o postdates commit

--- src/d/actor/d_a_item.cpp
+++ src/d/actor/d_a_item.cpp
@@ -308,6 +308,7 @@ BOOL daItem_c::_daItem_execute() {
 }
 
 /* 800F5834-800F59CC       .text mode_proc_call__8daItem_cFv */
+/* Nonmatching */
 void daItem_c::mode_proc_call() {
     static ModeFunc mode_proc[] = {
         &daItem_c::mode_wait,
