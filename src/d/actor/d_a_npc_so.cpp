cda8b6473b6ab4a8481dd112d9d45618a3a2c2a8
bandit31 <bandit31@bandit>
2026-08-13 09:16:47 +0300

    d_a_npc_so: transcribe hudeDraw 0->85.83 (tev color + stack copy anmMtx(11) + baseTRMtx + modelUpdateDL; rem = load-scheduling regalloc only); markers 83==83; .o 09:14

--- src/d/actor/d_a_npc_so.cpp
+++ src/d/actor/d_a_npc_so.cpp
@@ -954,6 +954,10 @@ void daNpc_So_c::debugDraw() {
 /* 000038E0-00003954       .text hudeDraw__10daNpc_So_cFv */
 void daNpc_So_c::hudeDraw() {
     /* Nonmatching */
+    g_env_light.setLightTevColorType(mpModel, &tevStr);
+    mDoMtx_stack_c::copy(mpMorf->getModel()->getAnmMtx(11));
+    mDoMtx_copy(mDoMtx_stack_c::now, mpModel->getBaseTRMtx());
+    mDoExt_modelUpdateDL(mpModel);
 }
 
 /* 00003954-00003B00       .text _draw__10daNpc_So_cFv */
