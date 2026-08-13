19bba4617be6610562deb4b2b3823ad9ef2573c2
bandit31 <bandit31@bandit>
2026-08-13 14:53:28 +0300

    d_a_obj_ekskz: transcribe all 9 fns (6@100, 3 sub-100 marked); .o 14:53:14
    CreateHeap 99.95 / Create 96.69 / Execute 98.42 remain (pool-slot anchor +
    dPa set stack-arg load-order holdouts); Draw 100 via 1-arg entry inline form.
    Mthd_Create/Mthd_Delete/Delete/set_mtx/init_mtx/ctor/Draw at 100.0 per-symbol.

--- include/d/actor/d_a_obj_ekskz.h
+++ include/d/actor/d_a_obj_ekskz.h
@@ -10,8 +10,19 @@
 namespace daObjEkskz {
     class Act_c : public dBgS_MoveBgActor {
     public:
-        void prm_get_swSave() const {}
-    
+        static const char M_arcname[6];
+        static Mtx M_tmp_mtx;
+
+        enum Prm_e {
+            PRM_SWSAVE_W = 0x08,
+            PRM_SWSAVE_S = 0x00,
+        };
+        s32 param_get_swSave() const {
+            return daObj::PrmAbstract<Prm_e>(this, PRM_SWSAVE_W, PRM_SWSAVE_S);
+        }
+
+        Act_c();
+
         virtual BOOL CreateHeap();
         virtual BOOL Create();
         cPhs_State Mthd_Create();
@@ -21,9 +32,10 @@ namespace daObjEkskz {
         void init_mtx();
         virtual BOOL Execute(Mtx**);
         virtual BOOL Draw();
-    
+
     public:
-        /* 0x2C8 */ u8 m2C8[0x2D0 - 0x2C8];
+        /* 0x2C4 Act_c vtable */
+        /* 0x2C8 */ request_of_phase_process_class mPhs;
         /* 0x2D0 */ J3DModel* mpModel;
         /* 0x2D4 */ dCcD_Stts mStts;
         /* 0x310 */ dCcD_Cyl mCyl;
@@ -34,9 +46,10 @@ namespace daObjEkskz {
         /* 0x480 */ u8 m480;
         /* 0x481 */ u8 m481[0x484 - 0x481];
         /* 0x484 */ dPa_smokeEcallBack mSmokeCallback[4];
-        /* 0x504 */ u8 m504[0x50C - 0x504];
+        /* 0x504 */ JPABaseEmitter* m504;
+        /* 0x508 */ JPABaseEmitter* m508;
         /* 0x50C */ JPABaseEmitter* mpSmoke0;
-        /* 0x510 */ u8 m510[0x514 - 0x510];
+        /* 0x510 */ JPABaseEmitter* m510;
         /* 0x514 */ dKy_tevstr_c mTevStr;
     };
 };
--- src/d/actor/d_a_obj_ekskz.cpp
+++ src/d/actor/d_a_obj_ekskz.cpp
@@ -6,6 +6,7 @@
 #include "d/dolzel_rel.h" // IWYU pragma: keep
 #include "d/actor/d_a_obj_ekskz.h"
 #include "d/d_cc_d.h"
+#include "res/Object/Ekskz.h"
 
 static dCcD_SrcCyl cyl_check_src = {
     // dCcD_SrcGObjInf
@@ -37,88 +38,237 @@ static dCcD_SrcCyl cyl_check_src = {
     }},
 };
 
-
-/* 00000078-00000368       .text CreateHeap__Q210daObjEkskz5Act_cFv */
-BOOL daObjEkskz::Act_c::CreateHeap() {
-    /* Nonmatching */
-}
-
-/* 00000368-0000054C       .text Create__Q210daObjEkskz5Act_cFv */
-BOOL daObjEkskz::Act_c::Create() {
-    /* Nonmatching */
-}
-
-/* 0000054C-00000684       .text Mthd_Create__Q210daObjEkskz5Act_cFv */
-cPhs_State daObjEkskz::Act_c::Mthd_Create() {
-    /* Nonmatching */
-}
-
-/* 00000C00-00000C64       .text Delete__Q210daObjEkskz5Act_cFv */
-BOOL daObjEkskz::Act_c::Delete() {
-    /* Nonmatching */
-}
-
-/* 00000C64-00000CBC       .text Mthd_Delete__Q210daObjEkskz5Act_cFv */
-BOOL daObjEkskz::Act_c::Mthd_Delete() {
-    /* Nonmatching */
-}
-
-/* 00000CBC-00000D68       .text set_mtx__Q210daObjEkskz5Act_cFv */
-void daObjEkskz::Act_c::set_mtx() {
-    /* Nonmatching */
-}
-
-/* 00000D68-00000DC0       .text init_mtx__Q210daObjEkskz5Act_cFv */
-void daObjEkskz::Act_c::init_mtx() {
-    /* Nonmatching */
-}
-
-/* 00000DC0-00000FFC       .text Execute__Q210daObjEkskz5Act_cFPPA3_A4_f */
-BOOL daObjEkskz::Act_c::Execute(Mtx**) {
-    /* Nonmatching */
-}
-
-/* 00000FFC-00001124       .text Draw__Q210daObjEkskz5Act_cFv */
-BOOL daObjEkskz::Act_c::Draw() {
-    /* Nonmatching */
-}
-
 namespace daObjEkskz {
-namespace {
-/* 00001124-00001144       .text Mthd_Create__Q210daObjEkskz27@unnamed@d_a_obj_ekskz_cpp@FPv */
-cPhs_State Mthd_Create(void* i_this) {
-    return ((daObjEkskz::Act_c*)i_this)->Mthd_Create();
-}
+    Mtx Act_c::M_tmp_mtx;
+    const char Act_c::M_arcname[] = "Ekskz";
 
-/* 00001144-00001164       .text Mthd_Delete__Q210daObjEkskz27@unnamed@d_a_obj_ekskz_cpp@FPv */
-BOOL Mthd_Delete(void* i_this) {
-    return ((daObjEkskz::Act_c*)i_this)->Mthd_Delete();
-}
+    /* 00000078-00000368       .text CreateHeap__Q210daObjEkskz5Act_cFv */
+    BOOL Act_c::CreateHeap() {
+        /* Nonmatching */
+        J3DModelData* model_data = (J3DModelData*)dComIfG_getObjectRes(M_arcname, dRes_INDEX_EKSKZ_BDL_EKSKZ_e);
+        JUT_ASSERT(144, model_data != NULL);
+        mpModel = mDoExt_J3DModel__create(model_data, 0, 0x11020203);
+        if (mpModel == NULL) {
+            return FALSE;
+        }
 
-/* 00001164-00001184       .text Mthd_Execute__Q210daObjEkskz27@unnamed@d_a_obj_ekskz_cpp@FPv */
-BOOL Mthd_Execute(void* i_this) {
-    return ((daObjEkskz::Act_c*)i_this)->MoveBGExecute();
-}
+        J3DModelData* model_data_eff = (J3DModelData*)dComIfG_getObjectRes(M_arcname, dRes_INDEX_EKSKZ_BDL_YOCWD00_e);
+        JUT_ASSERT(155, model_data_eff != NULL);
+        mpModelEff = mDoExt_J3DModel__create(model_data_eff, 0, 0x11020203);
+        if (mpModelEff == NULL) {
+            return FALSE;
+        }
 
-/* 00001184-000011B0       .text Mthd_Draw__Q210daObjEkskz27@unnamed@d_a_obj_ekskz_cpp@FPv */
-BOOL Mthd_Draw(void* i_this) {
-    return ((daObjEkskz::Act_c*)i_this)->Draw();
-}
+        J3DAnmTransform* bck = (J3DAnmTransform*)dComIfG_getObjectRes(M_arcname, dRes_INDEX_EKSKZ_BCK_YOCWD00_e);
+        JUT_ASSERT(164, bck != NULL);
+        if (!mBckAnm.init(model_data_eff, bck, TRUE, J3DFrameCtrl::EMode_LOOP, 1.0f, 0, -1, false)) {
+            return FALSE;
+        }
 
-/* 000011B0-000011DC       .text Mthd_IsDelete__Q210daObjEkskz27@unnamed@d_a_obj_ekskz_cpp@FPv */
-BOOL Mthd_IsDelete(void* i_this) {
-    return ((daObjEkskz::Act_c*)i_this)->IsDelete();
-}
+        J3DAnmTextureSRTKey* btk = (J3DAnmTextureSRTKey*)dComIfG_getObjectRes(M_arcname, dRes_INDEX_EKSKZ_BTK_YOCWD00_e);
+        JUT_ASSERT(171, btk != NULL);
+        if (!mBtkAnm.init(model_data_eff, btk, TRUE, J3DFrameCtrl::EMode_LOOP, 1.0f, 0, -1, false, FALSE)) {
+            return FALSE;
+        }
 
-static actor_method_class Mthd_Ekskz = {
-    (process_method_func)Mthd_Create,
-    (process_method_func)Mthd_Delete,
-    (process_method_func)Mthd_Execute,
-    (process_method_func)Mthd_IsDelete,
-    (process_method_func)Mthd_Draw,
-};
-}; // namespace
-}; // namespace daObjEkskz
+        J3DAnmTevRegKey* brk = (J3DAnmTevRegKey*)dComIfG_getObjectRes(M_arcname, dRes_INDEX_EKSKZ_BRK_YOCWD00_e);
+        JUT_ASSERT(178, brk != NULL);
+        if (!mBrkAnm.init(model_data_eff, brk, TRUE, J3DFrameCtrl::EMode_NONE, 1.0f, 0, -1, false, FALSE)) {
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    /* 00000368-0000054C       .text Create__Q210daObjEkskz5Act_cFv */
+    BOOL Act_c::Create() {
+        /* Nonmatching */
+        fopAcM_SetMtx(this, mpModel->getBaseTRMtx());
+        init_mtx();
+        fopAcM_setCullSizeBox(this, -4000.0f, -500.0f, -4000.0f, 4000.0f, 500.0f, 4000.0f);
+
+        mStts.Init(0xFF, 0xFF, this);
+        mCyl.Set(cyl_check_src);
+        mCyl.SetC(current.pos);
+        mCyl.SetStts(&mStts);
+        m480 = 0;
+        dKy_tevstr_init(&mTevStr, home.roomNo, 0xFF);
+        s8 roomNo0 = current.roomNo;
+        dComIfGp_particle_setToon(0xA2C2, &current.pos, &current.angle, NULL, 0xA0, &mSmokeCallback[0],
+                                  roomNo0);
+        s8 roomNo1 = current.roomNo;
+        dComIfGp_particle_setToon(0xA2C3, &current.pos, &current.angle, NULL, 0xA0, &mSmokeCallback[1],
+                                  roomNo1);
+
+        if (mSmokeCallback[0].getEmitter() != NULL) {
+            mSmokeCallback[0].getEmitter()->setGlobalPrmColor(mTevStr.mColorK0.r, mTevStr.mColorK0.g,
+                                                              mTevStr.mColorK0.b);
+            mSmokeCallback[0].getEmitter()->setGlobalEnvColor(mTevStr.mColorK0.r, mTevStr.mColorK0.g,
+                                                              mTevStr.mColorK0.b);
+        }
+        if (mSmokeCallback[1].getEmitter() != NULL) {
+            mSmokeCallback[1].getEmitter()->setGlobalPrmColor(mTevStr.mColorK0.r, mTevStr.mColorK0.g,
+                                                              mTevStr.mColorK0.b);
+            mSmokeCallback[1].getEmitter()->setGlobalEnvColor(mTevStr.mColorK0.r, mTevStr.mColorK0.g,
+                                                              mTevStr.mColorK0.b);
+        }
+
+        attention_info.distances[0] = 0x31;
+        attention_info.position.y = current.pos.y + 200.0f;
+        eyePos.y = attention_info.position.y;
+        return TRUE;
+    }
+
+    /* 0000054C-00000684       .text Mthd_Create__Q210daObjEkskz5Act_cFv */
+    cPhs_State Act_c::Mthd_Create() {
+        fopAcM_ct(this, Act_c);
+        m504 = NULL;
+        m508 = NULL;
+        mpSmoke0 = NULL;
+        m510 = NULL;
+
+        s32 swSave = param_get_swSave();
+        if (dComIfGs_isSwitch(swSave, home.roomNo)) {
+            return cPhs_STOP_e;
+        }
+        cPhs_State phase_state = dComIfG_resLoad(&mPhs, M_arcname);
+        if (phase_state == cPhs_COMPLEATE_e) {
+            phase_state = MoveBGCreate(M_arcname, dRes_INDEX_EKSKZ_DZB_EKSKZ_e, NULL, 0x28A0);
+            JUT_ASSERT(318, (phase_state == cPhs_COMPLEATE_e) || (phase_state == cPhs_ERROR_e));
+        }
+        return phase_state;
+    }
+
+    Act_c::Act_c() {}
+
+    /* 00000C00-00000C64       .text Delete__Q210daObjEkskz5Act_cFv */
+    BOOL Act_c::Delete() {
+        for (int i = 0; i < 4; i++) {
+            mSmokeCallback[i].remove();
+        }
+        return TRUE;
+    }
+
+    /* 00000C64-00000CBC       .text Mthd_Delete__Q210daObjEkskz5Act_cFv */
+    BOOL Act_c::Mthd_Delete() {
+        s32 result = MoveBGDelete();
+        if (fpcM_CreateResult(this) != cPhs_STOP_e) {
+            dComIfG_resDelete(&mPhs, M_arcname);
+        }
+        return result;
+    }
+
+    /* 00000CBC-00000D68       .text set_mtx__Q210daObjEkskz5Act_cFv */
+    void Act_c::set_mtx() {
+        shape_angle = current.angle;
+        mDoMtx_stack_c::transS(current.pos);
+        mDoMtx_stack_c::ZXYrotM(shape_angle);
+        mpModel->setBaseTRMtx(mDoMtx_stack_c::get());
+        mpModelEff->setBaseTRMtx(mDoMtx_stack_c::get());
+        mDoMtx_copy(mDoMtx_stack_c::get(), M_tmp_mtx);
+    }
+
+    /* 00000D68-00000DC0       .text init_mtx__Q210daObjEkskz5Act_cFv */
+    void Act_c::init_mtx() {
+        mpModel->setBaseScale(scale);
+        mpModelEff->setBaseScale(scale);
+        set_mtx();
+    }
+
+    /* 00000DC0-00000FFC       .text Execute__Q210daObjEkskz5Act_cFPPA3_A4_f */
+    BOOL Act_c::Execute(Mtx** i_mtx) {
+        /* Nonmatching */
+        attention_info.flags |= 1;
+        mBckAnm.play();
+        mBtkAnm.play();
+        dComIfG_Ccsp()->Set(&mCyl);
+        if (m480 == 0 && mCyl.ChkTgHit()) {
+            JAIZelBasic::zel_basic->seStart(0x806, NULL, 0, 0, 200.0f, 200.0f, -1.0f, -1.0f, 0);
+            fopAcM_onSwitch(this, param_get_swSave());
+            m480 = 1;
+            mSmokeCallback[0].end();
+            mSmokeCallback[1].end();
+
+            u8 r = tevStr.mColorK0.r;
+            u8 g = tevStr.mColorK0.g;
+            u8 b = tevStr.mColorK0.b;
+            mpSmoke0 = dComIfGp_particle_set(0x82EC, &current.pos, &current.angle);
+            if (mpSmoke0 != NULL) {
+                mpSmoke0->setGlobalPrmColor(r, g, b);
+            }
+            mSmokeCallback[3].onWindOff();
+            mSmokeCallback[3].setFollowOff();
+            dComIfGp_particle_setToon(0xA2ED, &current.pos, &current.angle, NULL, 0x80, &mSmokeCallback[3],
+                                      current.roomNo);
+            mSmokeCallback[3].setTevStr(&mTevStr);
+        }
+        if (m480 == 1) {
+            mBrkAnm.play();
+            if (mBrkAnm.isStop()) {
+                fopAcM_delete(this);
+            }
+        }
+        set_mtx();
+        *i_mtx = &M_tmp_mtx;
+        return TRUE;
+    }
+
+    /* 00000FFC-00001124       .text Draw__Q210daObjEkskz5Act_cFv */
+    BOOL Act_c::Draw() {
+        g_env_light.settingTevStruct(TEV_TYPE_ACTOR, &current.pos, &mTevStr);
+        g_env_light.setLightTevColorType(mpModelEff, &mTevStr);
+        mBckAnm.entry(mpModelEff->getModelData());
+        mBtkAnm.entry(mpModelEff->getModelData());
+        mBrkAnm.entry(mpModelEff->getModelData());
+        mDoExt_modelUpdateDL(mpModelEff);
+
+        g_env_light.settingTevStruct(TEV_TYPE_BG0, &current.pos, &tevStr);
+        g_env_light.setLightTevColorType(mpModel, &tevStr);
+        j3dSys.setDrawBuffer(g_dComIfG_gameInfo.drawlist.mpOpaListBG, 0);
+        j3dSys.setDrawBuffer(g_dComIfG_gameInfo.drawlist.mpXluListBG, 1);
+        if (m480 == 0) {
+            mDoExt_modelUpdateDL(mpModel);
+        }
+        j3dSys.setDrawBuffer(g_dComIfG_gameInfo.drawlist.mpOpaList, 0);
+        j3dSys.setDrawBuffer(g_dComIfG_gameInfo.drawlist.mpXluList, 1);
+        return TRUE;
+    }
+
+    namespace {
+        /* 00001124-00001144       .text Mthd_Create__Q210daObjEkskz27@unnamed@d_a_obj_ekskz_cpp@FPv */
+        cPhs_State Mthd_Create(void* i_this) {
+            return ((Act_c*)i_this)->Mthd_Create();
+        }
+
+        /* 00001144-00001164       .text Mthd_Delete__Q210daObjEkskz27@unnamed@d_a_obj_ekskz_cpp@FPv */
+        BOOL Mthd_Delete(void* i_this) {
+            return ((Act_c*)i_this)->Mthd_Delete();
+        }
+
+        /* 00001164-00001184       .text Mthd_Execute__Q210daObjEkskz27@unnamed@d_a_obj_ekskz_cpp@FPv */
+        BOOL Mthd_Execute(void* i_this) {
+            return ((Act_c*)i_this)->MoveBGExecute();
+        }
+
+        /* 00001184-000011B0       .text Mthd_Draw__Q210daObjEkskz27@unnamed@d_a_obj_ekskz_cpp@FPv */
+        BOOL Mthd_Draw(void* i_this) {
+            return ((Act_c*)i_this)->Draw();
+        }
+
+        /* 000011B0-000011DC       .text Mthd_IsDelete__Q210daObjEkskz27@unnamed@d_a_obj_ekskz_cpp@FPv */
+        BOOL Mthd_IsDelete(void* i_this) {
+            return ((Act_c*)i_this)->IsDelete();
+        }
+
+        static actor_method_class Mthd_Ekskz = {
+            (process_method_func)Mthd_Create,
+            (process_method_func)Mthd_Delete,
+            (process_method_func)Mthd_Execute,
+            (process_method_func)Mthd_IsDelete,
+            (process_method_func)Mthd_Draw,
+        };
+    }; // namespace
+};     // namespace daObjEkskz
 
 actor_process_profile_definition g_profile_Obj_Ekskz = {
     /* Layer ID     */ fpcLy_CURRENT_e,
