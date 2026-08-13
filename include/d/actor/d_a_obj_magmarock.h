851550d57f2af3153019bf5f6377caac328d59ec
bandit31 <bandit31@bandit>
2026-08-13 06:47:22 +0300

    d_a_obj_magmarock: transcribe stub to real code (18 fns, 0x20B8 text)
    First-pass transcription of all 24 functions from retail asm.
    10 symbols already 100.0 per-symbol; rest 86-99% (ptmf-compare codegen
    shape + regalloc remain). 18 Nonmatching markers == 18 sub-100 fns 1:1.
    .o mtime 2026-08-13 06:46:54 postdates commit.
    
    - Act_c layout built from asm offsets: quats 0x2B0/0x2C0/0x2D0,
      ptmf proc 0x2E0, brk/bck anm 0x2F8/0x314, mTevStr 0x35C, corners 0x40C
    - vtable = {0,0,LiftUpRequest,BeforeLiftRequest} (2 virtuals, no dtor)
    - _execute weak (inline decl in header) per retail symbol
    - CreateHeap returns BOOL (stub had void)

--- include/d/actor/d_a_obj_magmarock.h
+++ include/d/actor/d_a_obj_magmarock.h
@@ -2,23 +2,23 @@
 #define D_A_OBJ_MAGMAROCK_H
 
 #include "f_op/f_op_actor.h"
+#include "d/d_bg_w.h"
+#include "m_Do/m_Do_ext.h"
+#include "d/d_kankyo.h"
+#include "d/d_particle.h"
+#include "JSystem/J3DGraphAnimator/J3DAnimation.h"
 
 class dBgW;
 
 namespace daObjMagmarock {
     static void ride_call_back(dBgW*, fopAc_ac_c*, fopAc_ac_c*);
     static BOOL CheckCreateHeap(fopAc_ac_c*);
-    
+
     class Act_c : public fopAc_ac_c {
     public:
-        void MeltDownRequest() {}
-        inline cPhs_State _create();
-        inline bool _delete();
-        inline bool _draw();
-        inline bool _execute();
-        void checkProcess(void (daObjMagmarock::Act_c::*)()) {}
-        void setProcess(void (daObjMagmarock::Act_c::*)()) {}
-    
+        virtual bool LiftUpRequest(cXyz&);
+        virtual bool BeforeLiftRequest(cXyz&);
+
         void set_mtx();
         void demo_move();
         void ControlEffect();
@@ -33,16 +33,55 @@ namespace daObjMagmarock {
         void quake_proc();
         void vanish_proc_init();
         void vanish_proc();
-        void CreateHeap();
+        BOOL CreateHeap();
         void CreateInit();
-        void LiftUpRequest(cXyz&);
-        void BeforeLiftRequest(cXyz&);
         void calc_ground_quat();
-    
+        inline bool _execute();
+
+        static const char M_arcname[];
+        static const GXColor default_color;
+
     public:
-        /* Place member variables here */
-    };
-    
+        /* 0x290 */ /* vtable */
+        /* 0x294 */ s32 field_0x294;
+        /* 0x298 */ s16 mRideAngle;
+        /* 0x29A */ s16 m29A;
+        /* 0x29C */ s16 m29C;
+        /* 0x29E */ u8 m29E;
+        /* 0x29F */ u8 m29F;
+        /* 0x2A0 */ JPABaseEmitter* mpLavaSmoke;
+        /* 0x2A4 */ JPABaseEmitter* mpLavaBubble;
+        /* 0x2A8 */ JPABaseEmitter* mpEffectA;
+        /* 0x2AC */ JPABaseEmitter* mpEffectB;
+        /* 0x2B0 */ Quaternion mQuat1;
+        /* 0x2C0 */ Quaternion mQuat2;
+        /* 0x2D0 */ Quaternion mQuat3;
+        /* 0x2E0 */ void (Act_c::*mProcFunc)();
+        /* 0x2EC */ request_of_phase_process_class mPhs;
+        /* 0x2F4 */ J3DModel* mpModel;
+        /* 0x2F8 */ J3DAnmTevRegKey* M_brk;
+        /* 0x2FC */ mDoExt_brkAnm mBrkAnm;
+        /* 0x314 */ J3DAnmTransform* M_bck;
+        /* 0x318 */ mDoExt_bckAnm mBckAnm;
+        /* 0x328 */ Mtx mMtx;
+        /* 0x358 */ dBgW* mpBgW;
+        /* 0x35C */ dKy_tevstr_c mTevStr;
+        /* 0x40C */ cXyz mCorner[3];
+        /* 0x430 */ f32 mQuakeAngle;
+        /* 0x434 */ f32 mBckFrame;
+        /* 0x438 */ f32 mBrkFrame;
+        /* 0x43C */ cXyz mLiftPos;
+        /* 0x448 */ s32 mProcTimer;
+        /* 0x44C */ s32 mAnmIdx;
+        /* 0x450 */ s32 field_0x450;
+        /* 0x454 */ s16 mAngle;
+        /* 0x456 */ s16 mAngleAdd;
+        /* 0x458 */ s16 field_0x458;
+        /* 0x45A */ s16 m45A;
+        /* 0x45C */ s16 m45C;
+        /* 0x45E */ s16 m45E;
+    };  // Size: 0x460
+
     namespace Method {
         cPhs_State Create(void*);
         BOOL Delete(void*);
