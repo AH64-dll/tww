433a46cf0bdaa7fc73857fa73cefa341a380fa39
bandit31 <bandit31@bandit>
2026-08-14 18:34:20 +0300

    FLIP #66: d_a_grid -> Object(Equivalent)

--- configure.py
+++ configure.py
@@ -598,7 +598,7 @@ config.libs = [
             Object(Matching,    "d/actor/d_a_demo00.cpp"),
             Object(MatchingFor("GZLJ01", "GZLE01", "GZLP01"),    "d/actor/d_a_disappear.cpp"),
             Object(Matching,    "d/actor/d_a_esa.cpp"),
-            Object(NonMatching, "d/actor/d_a_grid.cpp"),
+            Object(Equivalent, "d/actor/d_a_grid.cpp"),  # framework-Equivalent (value-parity, 7-shape allocator)
             Object(NonMatching, "d/actor/d_a_himo2.cpp"),
             Object(Matching,    "d/actor/d_a_hookshot.cpp"),
             Object(Matching,    "d/actor/d_a_ib.cpp"),
