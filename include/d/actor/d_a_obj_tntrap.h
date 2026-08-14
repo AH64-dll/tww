667bf00f9ec6fe6d37698f841778fba006bea386
bandit31 <bandit31@bandit>
2026-08-14 05:12:59 +0300

    d_a_obj_tntrap: full transcription stub-tier -> 18 fns @100.0 (33 non-weak): header verified layout 0x290-0xDEC (mStts/mTri[8]/mpBgW/mMtx/particle arrays/ptmf mProcFunc + Param_e swSave/swSave2/arg0/mapType); create_heap/set_mtx/demo procs/wrappers 100.0; 15 sub-100 in 70-99% band (markers 19==19 per-symbol, .o mtime 05:12:43 postdates). Fresh .o build/GZLE01/src/d/actor/d_a_obj_tntrap.o

--- include/d/actor/d_a_obj_tntrap.h
+++ include/d/actor/d_a_obj_tntrap.h
@@ -2,33 +2,53 @@
 #define D_A_OBJ_TNTRAP_H
 
 #include "f_op/f_op_actor.h"
+#include "d/d_a_obj.h"
+#include "d/d_cc_d.h"
+#include "d/d_bg_w.h"
+#include "JSystem/JParticle/JPAEmitter.h"
+#include "d/d_com_inf_game.h"
+#include "SSystem/SComponent/c_phase.h"
 
 class daObjTnTrap_c : public fopAc_ac_c {
 public:
-    void param_get_arg0() const {}
-    void param_get_mapType() const {}
-    void param_get_swSave() const {}
-    void param_get_swSave2() const {}
+    enum Param_e {
+        PRM_SWSAVE_W = 8,
+        PRM_SWSAVE_S = 0,
 
-    void chk_appear();
+        PRM_SWSAVE2_W = 8,
+        PRM_SWSAVE2_S = 8,
+
+        PRM_ARG0_W = 1,
+        PRM_ARG0_S = 0x10,
+
+        PRM_MAPTYPE_W = 2,
+        PRM_MAPTYPE_S = 0x11,
+    };
+
+    s32 param_get_swSave() const { return daObj::PrmAbstract(this, PRM_SWSAVE_W, PRM_SWSAVE_S); }
+    s32 param_get_swSave2() const { return daObj::PrmAbstract(this, PRM_SWSAVE2_W, PRM_SWSAVE2_S); }
+    s32 param_get_arg0() const { return daObj::PrmAbstract(this, PRM_ARG0_W, PRM_ARG0_S); }
+    s32 param_get_mapType() const { return daObj::PrmAbstract(this, PRM_MAPTYPE_W, PRM_MAPTYPE_S); }
+
+    BOOL chk_appear();
     void set_mtx();
-    void solidHeapCB(fopAc_ac_c*);
-    void create_heap();
-    void particle_set(int, float);
+    static BOOL solidHeapCB(fopAc_ac_c*);
+    BOOL create_heap();
+    void particle_set(int, f32);
     void particle_delete(int);
     void set_se();
     void set_tri(int);
-    void chk_event_flg();
+    BOOL chk_event_flg();
     void set_em_set_offsetY();
     cPhs_State _create();
     bool _delete();
-    void trap_off_wait_act_proc();
-    void trap_on_wait_act_proc();
-    void demo_regist_wait_act_proc();
-    void demo_wait_act_proc();
-    void demo_wait2_act_proc();
-    void demo_end_wait_act_proc();
-    void hide_wait_act_proc();
+    BOOL trap_off_wait_act_proc();
+    BOOL trap_on_wait_act_proc();
+    BOOL demo_regist_wait_act_proc();
+    BOOL demo_wait_act_proc();
+    BOOL demo_wait2_act_proc();
+    BOOL demo_end_wait_act_proc();
+    BOOL hide_wait_act_proc();
     void dummy_proc();
     void trap_off_wait_act_init_proc();
     void trap_on_wait_act_init_proc();
@@ -40,7 +60,24 @@ public:
     bool _draw();
 
 public:
-    /* Place member variables here */
-};
+    /* 0x290 */ request_of_phase_process_class mPhs;
+    /* 0x298 */ s32 mAppear;
+    /* 0x29C */ dCcD_Stts mStts;
+    /* 0x2D8 */ dCcD_Tri mTri[8];
+    /* 0xD58 */ cBgW* mpBgW;
+    /* 0xD5C */ Mtx mMtx;
+    /* 0xD8C */ JPABaseEmitter* mBallPrt[2][2];
+    /* 0xD9C */ JPABaseEmitter* mThunderPrt[2][3];
+    /* 0xDB4 */ BOOL (daObjTnTrap_c::*mProcFunc)();
+    /* 0xDC0 */ s32 mAction;
+    /* 0xDC4 */ u8 mAppearFlg;
+    /* 0xDC8 */ s32 mSwSave;
+    /* 0xDCC */ s32 mSwSave2;
+    /* 0xDD0 */ s32 mArg0;
+    /* 0xDD4 */ s32 mMapType;
+    /* 0xDD8 */ s16 mEvtIdx;
+    /* 0xDE0 */ u8 mPrtSetFlg[2];
+    /* 0xDE4 */ f32 mOffsetY[2];
+};  // Size: 0xDEC
 
 #endif /* D_A_OBJ_TNTRAP_H */
