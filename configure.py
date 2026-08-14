8ad8245c4eff7ed38591f2ef95af427dfc39ffc6
bandit31 <bandit31@bandit>
2026-08-14 18:37:35 +0300

    FLIP #66: d_a_warpmj -> ActorRel(Equivalent)

--- configure.py
+++ configure.py
@@ -1821,7 +1821,7 @@ config.libs = [
     ActorRel(Matching,    "d_a_warpfout"),
     ActorRel(Matching,    "d_a_warpgn"),
     ActorRel(Equivalent, "d_a_warpls"),  # 44/47 @100, 3 accept-class holdouts; rel-link proves NOT Matching
-    ActorRel(NonMatching, "d_a_warpmj"),
+    ActorRel(Equivalent, "d_a_warpmj"),  # regalloc/weak order
     ActorRel(NonMatching, "d_a_waterfall"),
     ActorRel(MatchingFor("GZLJ01", "GZLE01", "GZLP01"),    "d_a_windmill"),
     ActorRel(NonMatching, "d_a_wz"),
